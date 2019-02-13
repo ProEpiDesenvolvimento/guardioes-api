fa6197d1ebe73e07e552cd3448061d1d571898ea
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-02-13 10:00:20 -0200

    all good

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'admins/', to: "admin#index"
   devise_for :admins,
     path: 'admin/',
     path_names: {
@@ -10,5 +11,7 @@ Rails.application.routes.draw do
       sessions: 'session',
       registrations: 'registration'
     }
+
+    root to: "admin#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
