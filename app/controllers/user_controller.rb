0aa45fbb69cca7882541941d50a52cd755e03c30
puerto229 <vinicius.alves.porto@gmail.com>
2019-02-15 13:13:51 -0200

    Show user controller method

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,4 +5,22 @@ class UserController < ApplicationController
 
     render json: @users
   end
+
+   # GET /users/1
+   def show
+    render json: @user
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:user_name, :email, :birthdate, :country, :gender, :race, :is_professional, :app_id)
+    end
+
 end
