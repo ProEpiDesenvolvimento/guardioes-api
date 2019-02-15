40c2f751cc5ecdaeb4ff6a8e75807f91a308fbc7
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-02-15 10:10:47 -0200

    preparing to use api inside react

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,5 @@
 class UserController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   def index
     @users = User.all
 
--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: []
+    }
+  }
+  _fetchUsers = () => {
+      try {
+        fetch("users", {
+          method: 'GET',
+          headers: {
+            Accept: 'application/vnd.api+json'
+          }
+        })
+        .then(res => res.json())
+        .then(resJson => {
+          this.setState({users: resJson})
+        })
+      } catch(error) {console.log(error)}
+  }
+  
   render() {
     return (
-      <div className="App">
+      <div>
+        <button onClick={this._fetchUsers}/>
       </div>
     );
   }
