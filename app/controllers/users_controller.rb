6ba889ebefd54d4e8800f36274accc60c3ddf5be
Marcos Cabeceira <cabeceiramarcos@gmail.com>
2019-04-27 14:42:16 -0300

    fixed user#update asking password bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,14 +15,14 @@ class UsersController < ApplicationController
     render json: @user
   end
 
-    # PATCH/PUT /apps/1
-    def update
-      if @user.update(user_params)
-        render json: @user
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
+  # PATCH/PUT /apps/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
+  end
   
 
   def destroy
@@ -30,19 +30,20 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      if current_user.id != params[:id]
-        render json: { errors: [
-          detail: I18n.t("user.access_forbiden")
-        ]}
-      else
-        @user = User.find(current_user.id)
-      end
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    # if current_user.id != params[:id]
+    #   render json: { errors: [
+    #     detail: I18n.t("user.access_forbiden")
+    #   ]}
+    # else
+    #   @user = User.find(current_user.id)
+    # end
+    @user = User.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.require(:user).permit(:user_name, :email, :birthdate, :country, :gender, :race, :is_professional, :app_id)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.require(:user).permit(:user_name, :email, :birthdate, :country, :gender, :race, :is_professional, :app_id, :password)
+  end
 end
