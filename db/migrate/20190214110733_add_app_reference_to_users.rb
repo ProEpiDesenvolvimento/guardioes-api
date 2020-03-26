aa2386b22d88a97bde98face4ef9b8f00f9b5bb5
ViniciusPuerto <vinicius.alves.porto@gmail.com>
2020-03-26 17:18:58 -0300

    Deployed 6376fb9 with MkDocs version: 1.1

--- db/migrate/20190214110733_add_app_reference_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAppReferenceToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :users, :app, foreign_key: true
-  end
-end
--- db/migrate/20190329182828_add_contact_with_symptom_to_surveys.rb
+++
@@ -1,9 +0,0 @@
-class AddContactWithSymptomToSurveys < ActiveRecord::Migration[5.2]
-  def self.up
-    add_column :surveys, :contact_with_symptom, :boolean
-  end
-
-  def self.down
-    remove_column :surveys, :contact_with_symptom
-  end
-end
