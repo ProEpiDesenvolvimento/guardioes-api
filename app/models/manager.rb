9126c63e3365ad1e4d2c5604a96d831661f75405
ViniciusPuerto <vinicius.alves.porto@gmail.com>
2020-04-01 17:25:45 -0300

    all the missing requirements

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -2,5 +2,5 @@ class Manager < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: JWTBlacklist
 end
