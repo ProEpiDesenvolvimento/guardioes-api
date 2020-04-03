64caad712da38385dad15663b1881f94e590de9c
ViniciusPuerto <vinicius.alves.porto@gmail.com>
2020-04-03 15:27:28 -0300

    Added new attributes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :user_name, :email, :birthdate, :country, :gender, :race, :is_professional, :picture, :app_id, :city, :state, :identification_code
+  attributes :id, :user_name, :email, :birthdate, :country, :gender, :race, :is_professional, :picture, :app_id, :city, :state, :identification_code, :group_id, :risk_group
 
   belongs_to :app do
     link(:app) {app_url(object.app.id)}
