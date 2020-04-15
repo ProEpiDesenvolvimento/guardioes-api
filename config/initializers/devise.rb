eb03e1fa927a145aea59deb91f520be3447ae6d5
ViniciusPuerto <vinicius.alves.porto@gmail.com>
2020-04-14 21:01:24 -0300

    Jwt expiration time to 1mo

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 1.week.to_i
+    jwt.expiration_time = 1.month.to_i
   end
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
