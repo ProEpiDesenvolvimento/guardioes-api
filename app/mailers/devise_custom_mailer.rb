5e8ba1113a079e1f55a02814b3f5942ce46b1600
ViniciusPuerto <vinicius.alves.porto@gmail.com>
2020-05-15 14:17:49 -0300

    sending the email

--- app/mailers/devise_custom_mailer.rb
+++ app/mailers/devise_custom_mailer.rb
@@ -1,11 +1,16 @@
 class DeviseCustomMailer < Devise::Mailer
-  helper :application # gives access to all helpers defined within `application_helper`.
-  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
-  default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
-  # If there is an object in your application that returns a contact email, you can use it as follows
-  # Note that Devise passes a Devise::Mailer object to your proc, hence the parameter throwaway (*).
-  default from: ->(*) { Class.instance.email_address }
+    include Devise::Controllers::UrlHelpers
   
+    def confirmation_instructions(resource, token, _options = {})
+      confirmation_path = ENV["WEB_HOST"] + '/user/confirmation/' + resource.confirmation_token
+      
+      SendgridMailer.send(
+        resource.unconfirmed_email ? resource.unconfirmed_email : resource.email,
+        { "confirmAccountUrl": confirmation_path },
+        ""
+      )
+    end
+
     def reset_password_instructions(resource, token, _options = {})
         reset_path = 'gds://password/' + resource.reset_password_token
       
