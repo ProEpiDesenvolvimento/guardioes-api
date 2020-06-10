0de7aaa5593deb16b1af4372e6b0f3a46a69ff67
RenatoBrittoAraujo <renatomwbbritto@gmail.com>
2020-06-10 18:27:12 -0300

    Fix email password reset bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,17 +54,6 @@ class UsersController < ApplicationController
       render json: {error: true, message: "Token invalido"}, status: :bad_request
     end
   end
-
-
-  def reset_password
-    @user = User.find_by_email(params[:email])
-
-    if @user.present?
-      @user.send_reset_password_instructions
-    end
-
-    render json: :no_content, status: :ok
-  end
   
   def destroy
     @user.destroy!
