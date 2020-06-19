8a335885e0aa483bfaf1c6124ec6835bec57174d
João Gabriel Dourado Brandão <GabrielDourado@adm.local>
2020-06-19 17:34:02 -0300

    fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   # before_action :ensure_json_request
-  protect_from_forgery
+  # protect_from_forgery
   
   def ensure_json_request
       return if request.headers["Accept"] =~ /vnd\.api\+json/
