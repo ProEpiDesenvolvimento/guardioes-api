608d527e16dbfeeb8e19585e38ee08341fee58a0
Gabriel dos Santos Martins <gsmartins96@hotmail.com>
2020-06-24 11:45:46 -0300

    :bug: Created_at with correct timezone

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,9 @@ module Myapp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.time_zone = 'Brasilia'
+    config.active_record.default_timezone = :local
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
