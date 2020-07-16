fff25f9895942b16d4f3c8ded3c3abb5ac4fbdd4
Tiago <tsrrodrigues@outlook.com>
2020-07-16 12:56:21 -0300

    Adding risk_group to serializers

--- app/serializers/household_serializer.rb
+++ app/serializers/household_serializer.rb
@@ -1,4 +1,4 @@
 class HouseholdSerializer < ActiveModel::Serializer
-  attributes :id, :description, :birthdate, :country, :gender, :race, :kinship, :picture, :school_unit_id, :identification_code
+  attributes :id, :description, :birthdate, :country, :gender, :race, :kinship, :picture, :school_unit_id, :identification_code, :risk_group
   has_one :user
 end
