4dee0e6cb152bd4668db5dfdaecdc38d00854f51
Renato Britto Araujo <renatobritto@protonmail.com>
2020-07-31 14:01:36 -0300

    Add group_id to household serializer

--- app/serializers/household_serializer.rb
+++ app/serializers/household_serializer.rb
@@ -1,5 +1,5 @@
 class HouseholdSerializer < ActiveModel::Serializer
-  attributes :id, :description, :birthdate, :country, :gender, :race, :kinship, :picture, :school_unit_id, :identification_code, :risk_group, :group
+  attributes :id, :description, :birthdate, :country, :gender, :race, :kinship, :picture, :school_unit_id, :identification_code, :risk_group, :group, :group_id
   has_one :user
 
   def group
