04007c9dec691b2b63d0edcdcf6a62aaa28e82be
Renato Britto Araujo <renatobritto@protonmail.com>
2020-08-18 17:24:54 -0300

    Create hack for dynamic index naming

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,22 +3,10 @@ class Survey < ApplicationRecord
   # Index name for a survey is now:
   # classname_environment[if survey user has group, _groupmanagergroupname]
   # For these changes to take effect, a reindex of the entire database is needed
-  searchkick index_name: -> {
-    env = 'production' if Rails.env.production? 
-    env = 'development' if Rails.env.development? 
-    env = 'test' if Rails.env.test?
-    # The reason self is not used here is because self, in
-    # this context, refers to the empty object Survey.new
-    last_survey = Survey.last
-    if last_survey.user.group.nil?
-      return 'surveys_' + env
-    end
-    group_name = last_survey.user.group.group_manager.group_name
-    group_name.downcase!
-    group_name.gsub! ' ', '_'
-    return 'surveys_' + env + '_' + group_name
-  }
-    
+  # Don't touch searchkick's functionalities before reading the comment on
+  # the search_data function, in this file.
+  searchkick index_name: 'trash_data'
+
   belongs_to :user
   belongs_to :household, optional:true
   before_validation :reverse_geocode
@@ -30,41 +18,69 @@ class Survey < ApplicationRecord
   end
   
   reverse_geocoded_by :latitude, :longitude do |obj,results|
-    if geo = results.first
-      obj.city    = geo.city
-      obj.country = geo.country
-      obj.street  = geo.street
-      obj.state   = geo.state
-    end
   end
   
   def get_message
     @user_symptoms = []
     symptom.map { |symptom|
-    if Symptom.where(:description=>symptom).any?
-      @user_symptoms.append(Symptom.where(:description=>symptom)[0])
+      if Symptom.where(:description=>symptom).any?
+        @user_symptoms.append(Symptom.where(:description=>symptom)[0])
+      end
+    }
+    symptoms_and_syndromes_data = {}
+    symptom_messages = get_symptoms_messages
+    if symptom_messages.any?
+      symptoms_and_syndromes_data[:symptom_messages] = symptom_messages
     end
-  }
-  symptoms_and_syndromes_data = {}
-  symptom_messages = get_symptoms_messages
-  if symptom_messages.any?
-    symptoms_and_syndromes_data[:symptom_messages] = symptom_messages
-  end
-  top_3 = get_top_3_syndromes
-  if top_3.any?
-    symptoms_and_syndromes_data[:top_3] = top_3.map { |obj| { name: obj[:syndrome].description, percentage: obj[:likelyhood] }}
-    syndrome_message = top_3[0][:syndrome].message
-    if !syndrome_message.nil?
-      symptoms_and_syndromes_data[:top_syndrome_message] = syndrome_message || ''
+    top_3 = get_top_3_syndromes
+    if top_3.any?
+      symptoms_and_syndromes_data[:top_3] = top_3.map { |obj| { name: obj[:syndrome].description, percentage: obj[:likelyhood] }}
+      syndrome_message = top_3[0][:syndrome].message
+      if !syndrome_message.nil?
+        symptoms_and_syndromes_data[:top_syndrome_message] = syndrome_message || ''
+      end
     end
+    return symptoms_and_syndromes_data
   end
-  return symptoms_and_syndromes_data
-end
 
   scope :filter_by_user, ->(user) { where(user_id: user) }
 
   # Data that gets sent as fields for elastic indexes
   def search_data
+    # READ THIS BEFORE TOUCHING THE FOLLOWING IF CONDITION
+
+    # The following is a solution to dynamically assign index names (during runtime)
+    # THE SOLUTION IS A HACK. Working with the library searchkick was not an option because it does  
+    # not provide a way to select during object create and update runtime the name of the index we 
+    # want to send the data to. The workaroud is to run the indexing twice in the following way:
+
+    # Pre-condition: In searchkick's module inclusion, up there ^^, index name is set as 'trash_data'
+    # and @reindex_control is NOT true
+    
+    # 1. The first time though, @reindex_control will be not true, therefore it enter the if below
+    # 2. Now we set searchkick's static index name variable as the name we want
+    # 3. We now set @reindex_control as true
+    # 4. We call reindex and during reindexing, the program loop comes right back to this function
+    # 5. Now the reindexing is running and @@searchkick_index has the index name we want
+    # 6. We set the index to the "standard" name of 'trash_data' and @reindex_control to false,  
+    #    thus fulfilling the pre-condition for further object use
+    # 7. Reindex has ended, we now send to the index 'trash_data' the empty object {}
+
+    # This solution is terrible but I could not think of a better one as of now
+    # If you want to solve this better, read searchkick's source code: github.com/ankane/searchkick
+
+    if @reindex_control != true
+      @@searchkick_index = index_pattern_name
+      @reindex_control = true
+      self.reindex
+      return {}
+    else
+      @@searchkick_index = 'trash_data'
+      @reindex_control = false
+    end
+
+    # End of hack
+
     user = nil
     elastic_data = self.as_json() 
     if !self.household_id.nil?
@@ -84,6 +100,17 @@ end
       elastic_data[symptom.description] = self.symptom.include? symptom.description
     end
     return elastic_data 
+  end
+
+  def index_pattern_name
+    env = ENV['RAILS_ENV']
+    if self.user.group.nil?
+      return 'surveys_' + env
+    end
+    group_name = self.user.group.group_manager.group_name
+    group_name.downcase!
+    group_name.gsub! ' ', '-'
+    return 'surveys_' + env + '_' + group_name
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   acts_as_paranoid
   # Index name for a user is now:
   # classname_environment[if user has group, _groupmanagergroupname]
   # For these changes to take effect, a reindex of the entire database is needed
-  searchkick index_name: -> {
-    env = 'production' if Rails.env.production? 
-    env = 'development' if Rails.env.development? 
-    env = 'test' if Rails.env.test?
-    # The reason self is not used here is because self, in
-    # this context, refers to the empty object User.new
-    last_user = User.last
-    if last_user.group.nil?
-      return 'users_' + env
-    end
-    group_name = last_user.group.group_manager.group_name
-    group_name.downcase!
-    group_name.gsub! ' ', '_'
-    return 'users_' + env + '_' + group_name
-  }
+  # Don't touch searchkick's functionalities before reading the comment on
+  # the search_data function, in this file.
+  searchkick index_name: 'trash_data'
 
   has_many :households,
     dependent: :destroy
@@ -62,6 +48,40 @@ class User < ApplicationRecord
 
   # Data that gets sent as fields for elastic indexes
   def search_data
+    # READ THIS BEFORE TOUCHING THE FOLLOWING IF CONDITION
+
+    # The following is a solution to dynamically assign index names (during runtime)
+    # THE SOLUTION IS A HACK. Working with the library searchkick was not an option because it does  
+    # not provide a way to select during object create and update runtime the name of the index we 
+    # want to send the data to. The workaroud is to run the indexing twice in the following way:
+
+    # Pre-condition: In searchkick's module inclusion, up there ^^, index name is set as 'trash_data'
+    # and @reindex_control is NOT true
+    
+    # 1. The first time though, @reindex_control will be not true, therefore it enter the if below
+    # 2. Now we set searchkick's static index name variable as the name we want
+    # 3. We now set @reindex_control as true
+    # 4. We call reindex and during reindexing, the program loop comes right back to this function
+    # 5. Now the reindexing is running and @@searchkick_index has the index name we want
+    # 6. We set the index to the "standard" name of 'trash_data' and @reindex_control to false,  
+    #    thus fulfilling the pre-condition for further object use
+    # 7. Reindex has ended, we now send to the index 'trash_data' the empty object {}
+
+    # This solution is terrible but I could not think of a better one as of now
+    # If you want to solve this better, read searchkick's source code: github.com/ankane/searchkick
+
+    if @reindex_control != true
+      @@searchkick_index = index_pattern_name
+      @reindex_control = true
+      self.reindex
+      return {}
+    else
+      @@searchkick_index = 'trash_data'
+      @reindex_control = false
+    end
+
+    # End of hack
+
     elastic_data = self.as_json(except:['app_id', 'group_id', 'aux_code', 'reset_password_token'])
     elastic_data[:app] = self.app.app_name
     if !self.group.nil?
@@ -76,5 +96,16 @@ class User < ApplicationRecord
     end
     elastic_data[:household_count] = self.households.count
     return elastic_data 
+  end
+
+  def index_pattern_name
+    env = ENV['RAILS_ENV']
+    if self.group.nil?
+      return 'users_' + env
+    end
+    group_name = self.group.group_manager.group_name
+    group_name.downcase!
+    group_name.gsub! ' ', '-'
+    return 'users_' + env + '_' + group_name
   end
 end
