9fa1179b3b216b856ced7e972e8b110e0d9e2486
Italo Alves <italooko@gmail.com>
2020-09-02 09:56:08 -0300

    Add created_at to Household object

--- app/serializers/household_serializer.rb
+++ app/serializers/household_serializer.rb
@@ -1,5 +1,5 @@
 class HouseholdSerializer < ActiveModel::Serializer
-  attributes :id, :description, :birthdate, :country, :gender, :race, :kinship, :picture, :school_unit_id, :identification_code, :risk_group, :group, :group_id
+  attributes :id, :description, :birthdate, :country, :gender, :race, :kinship, :picture, :school_unit_id, :identification_code, :group, :group_id, :risk_group, :created_at
   has_one :user
 
   def group
