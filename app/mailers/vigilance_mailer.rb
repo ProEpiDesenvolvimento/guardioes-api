07baf729380ade8620419383843486e5e25b389e
tsrrodrigues <tsrrodrigues@outlook.com>
2020-09-25 12:58:52 -0300

    Getting group_manager vigilance_email correctly

--- app/mailers/vigilance_mailer.rb
+++ app/mailers/vigilance_mailer.rb
@@ -5,14 +5,14 @@ class VigilanceMailer < ActionMailer::Base
   def covid_vigilance_email(survey, user)
     @survey = survey
     
-    if user.group
-      group_manager = GroupManager.find_by_id(Group.find_by_id(user.group_id).id).vigilance_email
-      
+    if user.group_id
+      group_manager = user.group.group_manager
+
       email = mail()
       email.from = 'ProEpi <proepi.desenvolvimento@gmail.com>'
-      email.to = group_manager.group_name + ' <' + grou_manager.vigilance_email + '>'
+      email.to = group_manager.group_name + ' <' + group_manager.vigilance_email + '>'
       email.subject = '[VIGILANCIA ATIVA] Novo usuário com suspeita'
-      
+
       return email
     end
   end
