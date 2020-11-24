791d4589063c1d2e65b93c647e9c90d01cc2f559
Yoshida-Eduardo <edry97@hotmail.com>
2020-11-24 12:09:03 -0300

    Changes set_app method on registration controller Incoming changes reduce code duplication and cognitive complexity

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -23,34 +23,15 @@ class RegistrationController < Devise::RegistrationsController
   private
   def set_app
     if params[:user]
-      if params[:user][:residence].blank?
-        find_app = App.where(owner_country: params[:user][:country])
-  
-        if find_app.blank?
-          name = params[:user][:country]
-          app = App.create!(app_name: name, owner_country: name)
-  
-          puts "\n New Sign up Params App doesn't exist \n\n\n"
-          puts @new_sign_up_params
-          @new_sign_up_params = sign_up_params.merge(app_id: app.id).except(:residence)
-          puts "\n\n\n"
-        else
-          puts "\n New Sign up Params App does exist \n\n\n"
-          puts @new_sign_up_params
-          @new_sign_up_params = sign_up_params.merge(app_id: find_app.first.id).except(:residence)
-          puts "\n\n\n"
-        end
+      name = params[:user][:residence] || params[:user][:country]
+      
+      find_app = App.where(owner_country: name)
+
+      if find_app.blank?
+        app = App.create!(app_name: name, owner_country: name)
+        @new_sign_up_params = sign_up_params.merge(app_id: app.id).except(:residence)
       else
-        find_app = App.where(owner_country: params[:user][:residence])
-  
-        if find_app.blank?
-          name = params[:user][:residence]
-          app = App.create!(app_name: name, owner_country: name, twitter: nil)
-  
-          @new_sign_up_params = sign_up_params.merge(app_id: app.id).except(:residence)
-        else
-          @new_sign_up_params = sign_up_params.merge(app_id: find_app.first.id).except(:residence)
-        end
+        @new_sign_up_params = sign_up_params.merge(app_id: find_app.first.id).except(:residence)
       end
     end
   end
