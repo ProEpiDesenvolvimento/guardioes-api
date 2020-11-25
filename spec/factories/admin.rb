fe3bec8871df9b15e7da2a51e981b36f8a971d58
Yoshida-Eduardo <edry97@hotmail.com>
2020-11-25 16:58:27 -0300

    Minor fixes to factories

--- spec/factories/admin.rb
+++ spec/factories/admin.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
-    factory :admin do
-      email { Faker::Internet.email }
-      password { "12345678" }
-      first_name { Faker::Name.first_name }
-      last_name { Faker::Name.last_name }
-      is_god { true }
-      app { FactoryBot.create(:app)}
-    end
+  factory :admin do
+    email { Faker::Internet.email }
+    password { "12345678" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    is_god { true }
+    app { FactoryBot.create(:app)}
+  end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
-    factory :user do
-      email { Faker::Internet.email }
-      password { "12345678" }
-      user_name { Faker::Name.name }
-      email { Faker::Internet.email }
-      birthdate { Faker::Date.birthday(18, 65) }
-      country { Faker::Address.country }
-      gender { Faker::Gender.type }
-      race { "human" }
-      is_professional { false }
-      app { App.all.first }
-    end
+  factory :user do
+    email { Faker::Internet.email }
+    password { "12345678" }
+    user_name { Faker::Name.name }
+    birthdate { Faker::Date.birthday(18, 65) }
+    country { Faker::Address.country }
+    gender { Faker::Gender.type }
+    race { "human" }
+    is_professional { false }
+    app_id { App.all.first }
+  end
 end
