19a4050c0c3e4b27b7b60784662be71c4de5f53a
Yoshida-Eduardo <edry97@hotmail.com>
2020-11-25 17:39:22 -0300

    WIP

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,6 +4,9 @@ class GroupsController < ApplicationController
   before_action :validate_invalid_group_name, only: [:create,:update]
   before_action :authenticate_admin!, only: [:build_country_city_state_groups]
 
+  MANDATORY_COLUMNS = ["CODIGO","UNIDADE","ENDEREÇO","CEP","TELEFONE","EMAIL","PAIS","ESTADO","MUNICIPIO"]
+  MUNICIPALITY_INDEX = 8
+  DEFAULT_FILTERS_SIZE = 3
   # GET /groups
   def index
     @groups = Group.all
@@ -68,13 +71,10 @@ class GroupsController < ApplicationController
 
   # POST /groups/upload_group_file/
   def upload_group_file(build_country_city_state_model = false)
-    if !build_country_city_state_model && current_group_manager.nil?
-      return render json: 'You must be a group manager to create new groups'
-    end
+    validate_group_manager(build_country_city_state_model); return if performed?
     data = Roo::Spreadsheet.open(params[:file], extension: :xls) # Open spreadsheet data
 
     # Mandatory columns for .xls document to register new institutions
-    @mandatory_columns = ["CODIGO","UNIDADE","ENDEREÇO","CEP","TELEFONE","EMAIL","PAIS","ESTADO","MUNICIPIO"]
 
     headers = data.row(1) # First row containing headers for table
     
@@ -85,22 +85,12 @@ class GroupsController < ApplicationController
     end
     
     filter_columns = filter_columns headers
-
-    # 3 from the 'country, state, city' filting model + the size of custom filters
-    filter_size = 3 + filter_columns.length
+    filter_size = DEFAULT_FILTERS_SIZE + filter_columns.length
 
     rows_to_create = []
 
     data.each_with_pagename do |name, sheet|
-      sheet_data = sheet.as_json
-      sheet_data.each_with_index do |line, i|
-        line.each_with_index do |text, j|
-          if text.is_a? String
-            sheet_data[i][j] = text.strip
-          end
-        end
-      end
-
+      sheet_data = format_sheet sheet
       sheet.each_with_index(
         code: 'CODIGO', 
         description: 'UNIDADE',
@@ -117,51 +107,12 @@ class GroupsController < ApplicationController
 
           # List of path filters (country, state, municipality, filter_1, filter_2, ...)
           path_filter_list = (6..6+filter_size-1)
+          path = []
 
           # Check if any filter data is empty and returns error if so
           path_filter_list.each do |i|
-            if !build_country_city_state_model && (row_data[i].nil? || row_data[i].empty?)
-              return render json: {
-                message: 'Filter data is empty',
-                row: idx,
-                column: headers[i],
-                error: true
-              }, status: :unprocessable_entity
-            elif build_country_city_state_model && (!row_data[i].nil? || !row_data[i].empty?)
-              return render json: {
-                message: 'Filter data is filled',
-                row: idx,
-                column: headers[i],
-                error: true
-              }, status: :unprocessable_entity
-            end
-          end
-
-          path = []
-          path_filter_list.each do |i|
-            # If some group is called root_node, a forbidden name, return failure
-            if row_data[i] == 'root_node'
-              return render json: {message: 'You cannot name a group \'root_node\'', error: true}, status: :unprocessable_entity
-            end
-            # index 8 is 'municipality'
-            if i == 8 && !build_country_city_state_model
-              path << {
-                label: headers[i],
-                description: row_data[i],
-                children_label: 'GRUPO'
-              }
-              path << {
-                label: 'GRUPO',
-                description: current_group_manager.group_name,
-                children_label: headers[i + 1] || nil
-              }
-            else
-              path << {
-                label: headers[i],
-                description: row_data[i],
-                children_label: headers[i + 1] || nil
-              }
-            end
+            validate_row(build_country_city_state_model, row_data, i); return if performed?
+            path = fill_path row_data, headers, build_country_city_state_model, i
           end
 
           row = {
@@ -282,7 +233,7 @@ class GroupsController < ApplicationController
     # Check if all required columns are present and returns the one that aren't
     def validate_mandatory_columns(headers)
       not_found_columns = []
-      @mandatory_columns.each do |label|
+      MANDATORY_COLUMNS.each do |label|
         if !headers.include? label
           not_found_columns << label
         end
@@ -294,7 +245,7 @@ class GroupsController < ApplicationController
     def filter_columns(headers)
       filter_columns = []
       headers.each do |header|
-        if !@mandatory_columns.include? header
+        if !MANDATORY_COLUMNS.include? header
           filter_columns << header
         end
       end
@@ -319,4 +270,100 @@ class GroupsController < ApplicationController
         return render json: 'You cannot name a group \'root_node\'', status: :unprocessable_entity
       end
     end
-end
+
+    def format_sheet(sheet)
+      sheet_data = sheet.as_json
+      sheet_data.each_with_index do |line, i|
+        line.each_with_index do |text, j|
+          if text.is_a? String
+            sheet_data[i][j] = text.strip
+          end
+        end
+      end
+      sheet_data
+    end
+
+    def validate_row(build_country_city_state_model, row_data, i)
+      if !build_country_city_state_model && (row_data[i].nil? || row_data[i].empty?)
+        render json: {
+          message: 'Filter data is empty',
+          row: idx,
+          column: headers[i],
+          error: true
+        }, status: :unprocessable_entity and return
+      elif build_country_city_state_model && (!row_data[i].nil? || !row_data[i].empty?)
+        render json: {
+          message: 'Filter data is filled',
+          row: idx,
+          column: headers[i],
+          error: true
+        }, status: :unprocessable_entity and return
+      end
+      # If some group is called root_node, a forbidden name, return failure
+      if row_data[i] == 'root_node'
+        render json: {message: 'You cannot name a group \'root_node\'', error: true}, status: :unprocessable_entity and return
+      end
+    end
+
+    def fill_path(row_data, headers, build_country_city_state_model, i)
+      path = []
+      if i == MUNICIPALITY_INDEX && !build_country_city_state_model
+        path << {
+          label: headers[i],
+          description: row_data[i],
+          children_label: 'GRUPO'
+        }
+        path << {
+          label: 'GRUPO',
+          description: current_group_manager.group_name,
+          children_label: headers[i + 1] || nil
+        }
+      else
+        path << {
+          label: headers[i],
+          description: row_data[i],
+          children_label: headers[i + 1] || nil
+        }
+      end
+    end
+
+    def get_rows
+      sheet.each_with_index(
+        code: 'CODIGO', 
+        description: 'UNIDADE',
+        address: 'ENDEREÇO', 
+        cep: 'CEP', 
+        phone: 'TELEFONE',
+        email: 'EMAIL',
+      ) do |group_data, idx|
+          # Jump table header
+          next if idx == 0
+
+          # Get row from sheet
+          row_data = sheet_data[idx]
+
+          # List of path filters (country, state, municipality, filter_1, filter_2, ...)
+          path_filter_list = (6..6+filter_size-1)
+          path = []
+
+          # Check if any filter data is empty and returns error if so
+          path_filter_list.each do |i|
+            validate_row(build_country_city_state_model, row_data, i); return if performed?
+            path = fill_path row_data, headers, build_country_city_state_model, i
+          end
+
+          row = {
+            metadata: group_data,
+            path: path
+          }
+
+          rows_to_create << row
+        end
+    end
+
+    def validate_group_manager(build_country_city_state_model)
+      if !build_country_city_state_model && current_group_manager.nil?
+        render json: 'You must be a group manager to create new groups', status: :forbidden
+      end
+    end
+  end
