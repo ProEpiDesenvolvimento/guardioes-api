b3ec7001919596ff45e9adc720111e585948d657
Yoshida-Eduardo <edry97@hotmail.com>
2020-11-25 16:17:09 -0300

    Adds manager login macro

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,17 +1,25 @@
 module ControllerMacros
-    def login_admin
-      before(:each) do
-        @request.env["devise.mapping"] = Devise.mappings[:admin]
-        sign_in FactoryBot.create(:admin) # Using factory bot as an example
-      end
-    end
-  
-    def login_user
-      before(:each) do
-        @request.env["devise.mapping"] = Devise.mappings[:user]
-        user = FactoryBot.create(:user)
-        user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
-        sign_in user
-      end
+  def login_admin
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in FactoryBot.create(:admin) # Using factory bot as an example
     end
   end
+
+  def login_user
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      user = FactoryBot.create(:user)
+      user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
+      sign_in user
+    end
+  end
+
+  def login_group_manager
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:group_manager]
+      user = FactoryBot.create(:group_manager)
+      sign_in user
+    end
+  end
+end
