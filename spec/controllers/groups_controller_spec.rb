54829a75db5f5f22e0091ab21399a14df9b451d1
Yoshida-Eduardo <edry97@hotmail.com>
2020-11-25 16:18:10 -0300

    Adds create group happy path test

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe GroupsController, type: :controller do
+  let(:valid_session) { {} }
+  login_group_manager
 
+  before :each do
+    @file = fixture_file_upload('../fixtures/file.xls', 'application/vnd.ms-excel')
+  end
+  describe 'POST groups#updload_group_file' do
+    it "creates groups" do
+      post :upload_group_file, params: { file: @file, build_country_city_state_model: true }, session: valid_session
+      expect(response).to have_http_status(:created)
+    end
+  end
 end
