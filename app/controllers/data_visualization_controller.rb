0ab837c7ad8cecaecd36b30ae88e1566f65c5852
Icaro Oliveira <icarooliv2@gmail.com>
2020-12-06 14:20:07 -0300

    prevents StrongParams to block json keys

--- app/controllers/data_visualization_controller.rb
+++ app/controllers/data_visualization_controller.rb
@@ -19,20 +19,23 @@ class DataVisualizationController < ApplicationController
 
   def metabase_urls
     metabase_config = Rails.application.config.metabase
+    req = JSON.parse(request.raw_post)
+
     iframe_urls = []
     exp_time = Time.now.to_i + metabase_config[:exp_time]
-    params['payloads'].each {
+
+    req['payloads'].each {
       | payload |
+        payload['exp'] = exp_time
         token = JWT.encode payload, metabase_config[:secret_key]
-        iframe_url = metabase_config[:site_url] + "/embed/question/" + token + "#bordered=true&titled=true"
         iframe_urls.append({
           :question => payload['resource']['question'],
-          :iframe_url => iframe_url
+          :iframe_url => "#{metabase_config[:site_url]}/embed/question/#{token}#bordered=true&titled=true"
         })
     }
-  
+
     return render json: {
       'urls': iframe_urls
     }
-    end
+  end
 end
