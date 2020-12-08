e71f0d954d221af6831d4d577c911043ca6fc10e
Andrew Lucas <andrewlucasgs@gmail.com>
2020-12-07 21:05:44 -0300

    Fix lint errors on apps controller

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Apps Controller
 class AppsController < ApplicationController
   before_action :authenticate_admin!, except: [:get_twitter]
   before_action :authenticate_admin_is_god, except: %i[update show get_twitter]
@@ -16,20 +17,20 @@ class AppsController < ApplicationController
   # GET /apps/1
   def show
     @admin = Admin.where(app_id: @app.id)
-    newApps = { app: @app }.merge({ admin: @admin })
-    render json: newApps
+    new_apps = { app: @app }.merge({ admin: @admin })
+    render json: new_apps
   end
 
   # POST /apps
   def create
-    if params[:access_apps_token] == Rails.application.credentials.access_to_apps || admin_signed_in?
-      @app = App.new(app_params)
+    return unless params[:access_apps_token] == Rails.application.credentials.access_to_apps || admin_signed_in?
 
-      if @app.save
-        render json: @app, status: :created, location: @app
-      else
-        render json: @app.errors, status: :unprocessable_entity
-      end
+    @app = App.new(app_params)
+
+    if @app.save
+      render json: @app, status: :created, location: @app
+    else
+      render json: @app.errors, status: :unprocessable_entity
     end
   end
 
@@ -48,7 +49,9 @@ class AppsController < ApplicationController
   end
 
   # GET /apps/:id/get_twitter
+  # rubocop:disable Naming/AccessorMethodName
   def get_twitter
+    # rubocop:enable Naming/AccessorMethodName
     render json: { twitter: @app.twitter }
   end
 
