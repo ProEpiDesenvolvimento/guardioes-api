515f51a9bc6d8219296bcd3c65b539b8dc12d231
jonatas1n <jonatasgeek@gmail.com>
2021-01-11 20:52:16 -0300

    Some trys

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::FileUpdateChecker
 
   config.metabase = {
-    :site_url => ENV['metabase_site_url'],
-    :secret_key => ENV['metabase_secret_key'],
+    :site_url => ENV['METABASE_SITE_URL'],
+    :secret_key => ENV['METABASE_SECRET_KEY'],
     :exp_time =>  (60 * 100) # 100 minute expiration
   }
 end
