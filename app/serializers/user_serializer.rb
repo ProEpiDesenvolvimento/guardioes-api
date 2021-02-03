c38b1a70375922830a58225f3a6b125b89cc91ec
Italo Alves <italooko@gmail.com>
2021-02-03 12:20:09 -0300

    Hide picture column to User and Household

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :user_name, :email, :birthdate, :country, :gender, :race, :is_professional, :picture, :city, :state, :identification_code, :group_id, :risk_group, :group, :streak, :policy_version, :created_at, :is_vigilance, :phone, :identification_code
+  attributes :id, :user_name, :email, :birthdate, :country, :gender, :race, :is_professional, :city, :state, :identification_code, :group_id, :risk_group, :group, :streak, :policy_version, :created_at, :is_vigilance, :phone
 
   belongs_to :app do
     link(:app) {app_url(object.app.id)}
