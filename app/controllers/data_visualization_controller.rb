6db8665822488e047359dea1a58945d86a628db6
jonatas1n <jonatasgeek@gmail.com>
2021-04-06 21:40:56 -0300

    Adds urls to iframe vector for dashboard tabs

--- app/controllers/data_visualization_controller.rb
+++ app/controllers/data_visualization_controller.rb
@@ -1,6 +1,8 @@
 require 'jwt'
 
 class DataVisualizationController < ApplicationController
+before_action :set_current_request_user, only: [:metabase_urls]
+
   def users_count
     return render json: User.count
   end
@@ -18,24 +20,73 @@ class DataVisualizationController < ApplicationController
   end
 
   def metabase_urls
+    case @current_request_user
+    when current_admin
+      payloadSurveys = {'params' => {'app' => @current_request_user.app_id}, 'resource' => {'dashboard' => 1}}
+      payloadUsers = {'params' => {'app' => @current_request_user.app_id}, 'resource' => {'dashboard' => 1}}
+      payloadBiosecurity = {'params' => {'app' => @current_request_user.app_id}, 'resource' => {'dashboard' => 1}}
+    when current_manager
+      payloadSurveys = {'params' => {'app' => @current_request_user.app_id}, 'resource' => {'dashboard' => 1}}
+      payloadUsers = {'params' => {'app' => @current_request_user.app_id}, 'resource' => {'dashboard' => 1}}
+      payloadBiosecurity = {'params' => {'app' => @current_request_user.app_id}, 'resource' => {'dashboard' => 1}}
+    when current_city_manager
+      payloadSurveys = {'params' => {'city' => @current_request_user.city}, 'resource' => {'dashboard' => 6}}
+      payloadUsers = {'params' => {'city' => @current_request_user.city}, 'resource' => {'dashboard' => 6}}
+      payloadBiosecurity = {'params' => {'city' => @current_request_user.city}, 'resource' => {'dashboard' => 6}}
+    when current_group_manager
+      payloadSurveys = {'params' => {'group' => @current_request_user.id}, 'resource' => {'dashboard' => 5}}
+      payloadUsers = {'params' => {'group' => @current_request_user.id}, 'resource' => {'dashboard' => 5}}
+      payloadBiosecurity = {'params' => {'group' => @current_request_user.id}, 'resource' => {'dashboard' => 5}}
+    when current_user
+      puts "Common user"
+    end
+
     metabase_config = Rails.application.config.metabase
     req = JSON.parse(request.raw_post)
 
     iframe_urls = []
     exp_time = Time.now.to_i + metabase_config[:exp_time]
 
-    req['payloads'].each {
-      | payload |
-        payload['exp'] = exp_time
-        token = JWT.encode payload, metabase_config[:secret_key]
-        iframe_urls.append({
-          :dashboard => payload['resource']['dashboard'],
-          :iframe_url => "#{metabase_config[:site_url]}/embed/dashboard/#{token}#bordered=true&titled=true"
-        })
-    }
+    payloadSurveys['exp'] = exp_time
+    payloadUsers['exp'] = exp_time
+    payloadBiosecurity['exp'] = exp_time
+
+    tokenSurveys = JWT.encode payloadSurveys, metabase_config[:secret_key]
+    tokenUsers = JWT.encode payloadUsers, metabase_config[:secret_key]
+    tokenBiosecurity = JWT.encode payloadBiosecurity, metabase_config[:secret_key]
+
+    iframe_urls.append({
+      :dashboard => payloadSurveys['resource']['dashboard'],
+      :iframe_url => "#{metabase_config[:site_url]}/embed/dashboard/#{tokenSurveys}#bordered=true&titled=true"
+    })
+
+    iframe_urls.append({
+      :dashboard => payloadUsers['resource']['dashboard'],
+      :iframe_url => "#{metabase_config[:site_url]}/embed/dashboard/#{tokenUsers}#bordered=true&titled=true"
+    })
+
+    iframe_urls.append({
+      :dashboard => payloadBiosecurity['resource']['dashboard'],
+      :iframe_url => "#{metabase_config[:site_url]}/embed/dashboard/#{tokenBiosecurity}#bordered=true&titled=true"
+    })
 
     return render json: {
       'urls': iframe_urls
     }
   end
+
+  private
+    def set_current_request_user
+      if not current_manager.nil?
+        @current_request_user = current_manager
+      elsif not current_group_manager.nil?
+        @current_request_user = current_group_manager
+      elsif not current_admin.nil?
+        @current_request_user = current_admin
+      elsif not current_city_manager.nil?
+        @current_request_user = current_city_manager
+      else
+        return render json: { errors: "Token not found" }, status: :unprocessable_entity 
+      end
+    end 
 end
