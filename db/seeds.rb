b9d8210616bb83a999c75e536f44d0016576c320
Joao Pedro <jps12carvalho@gmail.com>
2021-06-08 18:15:04 -0300

    Deleting seed.rb used

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,37 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-print "Creating App\n"
-
-App.create!(
-        app_name: 'app de teste',
-        owner_country: "Brasil",
-)
-
-# print "App finished, creating Admin now\n"
-
-# app = App.last
-# app.admins.create!(
-#     {
-#         email: 'jp@email.com',
-#         password: '12345678',
-#     }
-# )
-# print "Admin finished, creating user"
-
-# app = App.last
-# app.users.create!(
-#     email: "user@email.com",
-#     password: "12345678",
-#     user_name: "Julio",
-# )
-
-# print "User created, creating Survey"
-# user = User.last
-# user.surveys.create!(
-#     latitude: -15.876934377095463,
-#     longitude: -48.085217354527146,
-# )
-
-# print "Surveys created\n"
