59b6c934f1e61bf1bca7bb49a1a1d11434da2912
Joao Pedro <jps12carvalho@gmail.com>
2021-06-22 20:17:34 -0300

    Updating require_id field

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_06_05_233453) do
-
+ActiveRecord::Schema.define(version: 2021_06_22_214806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,7 +150,7 @@ ActiveRecord::Schema.define(version: 2021_06_05_233453) do
     t.bigint "app_id"
     t.string "group_name"
     t.string "twitter"
-    t.boolean "require_id"
+    t.string "require_id"
     t.integer "id_code_length"
     t.string "vigilance_email"
     t.string "aux_code"
