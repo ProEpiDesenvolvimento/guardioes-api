f44830e8d78b3ced9915ea606648f4daeed88df1
Joao Pedro <jps12carvalho@gmail.com>
2021-07-23 15:11:13 -0300

    Addiing validations to Vaccine model

--- app/models/vaccine.rb
+++ app/models/vaccine.rb
@@ -1,2 +1,45 @@
 class Vaccine < ApplicationRecord
+  belongs_to :app
+
+	validates :name,
+		presence: true,
+			length: {
+				minimum: 1,
+				maximum: 255
+		}
+
+	validates :country_origin,
+		presence: true,
+		length: {
+			minimum: 1,
+			maximum: 255
+		}
+	
+	validates :doses,
+		presence: true,
+		numericality: {
+			greater_than: 0,
+			less_than: 10,
+		}
+	
+	validates :laboratory,
+		presence: true,
+		length: {
+			minimum: 1,
+			maximum: 255
+		}
+
+	validates :max_dose_interval,
+		presence: true,
+		numericality: {
+			greater_than_or_equal_to: :min_dose_interval,
+			allow_nil: true
+		}
+	
+	validates :min_dose_interval,
+		presence: true,
+		numericality: {
+			less_than_or_equal_to: :max_dose_interval,
+			allow_nil: true
+		}
 end
