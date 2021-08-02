06e18363779634c0f85841b23123a396764601df
Joao Pedro <jps12carvalho@gmail.com>
2021-08-02 19:11:24 -0300

    Fixing group_manager serializer and adding first_access to group_manager_teams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_225603) do
+ActiveRecord::Schema.define(version: 2021_08_02_220538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -154,6 +154,7 @@ ActiveRecord::Schema.define(version: 2021_07_29_225603) do
     t.string "created_by"
     t.string "updated_by"
     t.string "deleted_by"
+    t.boolean "first_access", default: true
     t.index ["app_id"], name: "index_group_manager_teams_on_app_id"
     t.index ["deleted_at"], name: "index_group_manager_teams_on_deleted_at"
     t.index ["email"], name: "index_group_manager_teams_on_email", unique: true
