0239f530d7981bd740d02737ef3da1afd9f0dd55
Joao Pedro <jps12carvalho@gmail.com>
2021-09-02 10:47:45 -0300

    Removing debug prints

--- app/serializers/survey_for_map_serializer.rb
+++ app/serializers/survey_for_map_serializer.rb
@@ -4,10 +4,8 @@ class SurveyForMapSerializer < ActiveModel::Serializer
     def attributes(*args)
         h = super(*args)
         coordinates = Survey.new(h).get_anonymous_latitude_longitude
-        print h[:latitude].to_s, h[:longitude].to_s, '\n'
         h[:latitude] = coordinates[:latitude]
         h[:longitude] = coordinates[:longitude]
-        print h[:latitude].to_s, h[:longitude].to_s, '\n'
         h
     end
 end  
