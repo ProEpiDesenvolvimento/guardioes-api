1aa540857d3f0a92c52d3ba989f3e5d8ae02ab65
Ítalo Alves <italooko@gmail.com>
2021-09-06 13:11:18 -0300

    Keep group_permissions method and fix GroupManagerTeam relation

--- app/models/group_manager_team.rb
+++ app/models/group_manager_team.rb
@@ -6,13 +6,13 @@ class GroupManagerTeam < ApplicationRecord
 
   acts_as_paranoid
 
+  belongs_to :group_manager
+  belongs_to :app
+
   has_many :manager_group_permission, :class_name => 'ManagerGroupPermission', :through => :group_manager
   has_many :groups, :through => :manager_group_permission 
   has_one :permission, dependent: :destroy
   accepts_nested_attributes_for :permission
-
-  belongs_to :group_manager
-  belongs_to :app
 
   # Check if a group is permitted by recusively scaling group branch
   # confering if any of the groups are permitted on the way
