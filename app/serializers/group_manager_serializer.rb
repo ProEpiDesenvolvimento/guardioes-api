59bfe394a51d2fbe8223b5de580c58aa784497c8
Joao Pedro <jps12carvalho@gmail.com>
2021-10-19 14:27:22 -0300

    Adding function to debug group_manager_serializer

--- app/serializers/group_manager_serializer.rb
+++ app/serializers/group_manager_serializer.rb
@@ -14,4 +14,15 @@ class GroupManagerSerializer < ActiveModel::Serializer
       end
     end
   end
+
+  def group_permissions
+    list = []
+    object.groups.each do |g|
+      list << { 
+        group: g.get_path(string_only=true,labled=false).join('/'),
+        id: g.id 
+      }
+    end
+    list
+  end
 end
