44249fa44ea5a7191b0a0bf9d3d4beb4c9a01464
Eduardo Marques <eduardoreissk8@gmail.com>
2021-12-30 20:13:11 -0300

    add category permision in controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :update, :destroy]
+  load_and_authorize_resource
 
   # GET /categories
   def index
