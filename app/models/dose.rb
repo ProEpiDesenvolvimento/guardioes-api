14839a4322648cbb4689d02b2ef6336a5a434b4d
Ítalo Alves <italooko@gmail.com>
2022-03-01 13:33:58 -0300

    Fix dose migration method

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -12,15 +12,18 @@ class Dose < ApplicationRecord
   # Migrates doses from user attributes to doses table
   def self.migrate_doses
     User.where.not(vaccine_id: nil).each do |u|
+      vaccine_id = u.vaccine_id
+  
       if u.first_dose_date.present?
-        @dose_1 = Dose.new(date: u.first_dose_date, dose: 1, vaccine_id: u.vaccine_id, user_id: u.id)
+        @dose_1 = Dose.new(date: u.first_dose_date, dose: 1, vaccine_id: vaccine_id, user_id: u.id)
         if @dose_1.save
           u.update_attribute(:first_dose_date, nil)
+          u.update_attribute(:vaccine_id, nil)
         end
       end
 
       if u.second_dose_date.present?
-        @dose_2 = Dose.new(date: u.second_dose_date, dose: 2, vaccine_id: u.vaccine_id, user_id: u.id)
+        @dose_2 = Dose.new(date: u.second_dose_date, dose: 2, vaccine_id: vaccine_id, user_id: u.id)
         if @dose_2.save
           u.update_attribute(:second_dose_date, nil)
           u.update_attribute(:vaccine_id, nil)
