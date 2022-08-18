ef4f0b545de34d09de5ac9b10495088f1eb63557
phrezende-eng <ph.hr.001@gmail.com>
2022-08-18 01:44:07 -0300

    - improvement for user_mailer class

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,33 @@
-class UserMailer < ActionMailer::Base
-  default from: 'proepi.desenvolvimento@gmail.com'
-  layout 'mailer'
+def user_for_field_nill
+  return User.new(
+    :user_name => "",
+    :email => "",
+    :password => "",
+    :app_id => 1
+  )
+end
 
-  def reset_password_email(user)
+class UserMailer < ActionMailer::Base
+default from: 'proepi.desenvolvimento@gmail.com'
+layout 'mailer'
+
+def reset_password_email(user)
+  if user == nil
+    @user = user_for_field_nill()
+    email = mail()
+    email.subject = "Campo inválido, informe um usuário"
+    return email
+  end
+  else
     @user = user
     email = mail()
+    if user.user_name == nil || user.email == nil
+      email.subject = "Usuário inválido"
+      return email
+    end
     email.from = 'ProEpi <proepi.desenvolvimento@gmail.com>'
     email.to = user.user_name + ' <' + user.email + '>'
     email.subject = '[Guardiões da Saúde] Redefinir Senha'
     return email
-  end
+end
 end
